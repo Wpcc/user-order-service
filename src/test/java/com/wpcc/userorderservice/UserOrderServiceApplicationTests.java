ab1f42d08638ae2fc9b78ab50efc4fb33217ee22
wpcc <310624463@qq.com>
2026-08-27 11:37:31 +0800

    chore: initialize Spring Boot user order service

---
+++ src/test/java/com/wpcc/userorderservice/UserOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wpcc.userorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
